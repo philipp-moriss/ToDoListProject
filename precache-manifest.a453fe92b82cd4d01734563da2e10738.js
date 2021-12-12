d9daf3575acb6363a53ccb8767ba65438f9bae5d
philipp-moriss <filverem@gmail.com>
2021-12-12 16:29:24 +0300

    Updates

---
+++ precache-manifest.a453fe92b82cd4d01734563da2e10738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbcc099bf0b171f1bee39a980b6b33b",
+    "url": "/ToDoListProject/index.html"
+  },
+  {
+    "revision": "f777f37b58f1b33494a7",
+    "url": "/ToDoListProject/static/css/main.6da83bb5.chunk.css"
+  },
+  {
+    "revision": "089502940f72793fbec0",
+    "url": "/ToDoListProject/static/js/2.6c47adba.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/ToDoListProject/static/js/2.6c47adba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f777f37b58f1b33494a7",
+    "url": "/ToDoListProject/static/js/main.b4acbc1e.chunk.js"
+  },
+  {
+    "revision": "091ee7dfd2e234afa509",
+    "url": "/ToDoListProject/static/js/runtime-main.b9c9bf69.js"
+  }
+]);
